290f725ed2765ebc14386b1f7b5e29b9dbd61575
Nathaniel <nathaniel.1stacc@gmail.com>
2023-02-23 06:12:51 +0800

    add test builder

--- index.js
+++ index.js
@@ -2,23 +2,92 @@ const BD = new Builder()
 
 BD.createCustomElement({
     style: `
-        h3 { color: green }
+        h3 { 
+            color: green 
+        }
+        button {
+            background: red;
+            color: black;
+        }
+        p {
+            color: violet ;
+        }
     `,
     template: `
-    <h3>
-        <slot></slot>
-    </h3>
+        <div>
+            <h3>
+                <slot></slot>
+            </h3>
+            <button>buttn</button>
+            <p>hello</p>
+        </div>
     `,
     script: {
         initial: (root) => {
             console.log(root);
-        },
-        onMount: (root)=> {
-            console.log("hello");
-        }
+            root.querySelector('.somethinfg')
+        }, 
+        onMount: (root) => {
+            console.log(root);
+            root.querySelector('.somethinfg')
+            root.querySelector('.somethinfg')
+    
+        }, 
+        onDismount: (root) => {
+            console.log(root);
+            root.querySelector('.somethinfg')
+        }, 
+        onChangeAtt: (root) => {
+            console.log(root);
+            root.querySelector('.somethinfg')
+        }, 
     },
     data: {
         name: 'todo-list',
         attributes: []
     }
+})
+
+BD.createCustomElementtest({ 
+    template: `
+        <style>
+            h3 { 
+                color: green 
+            }
+            button {
+                background: red;
+                color: black;
+            }
+            p {
+                color: violet ;
+            }
+        </style>
+        <div>
+            <h3>
+                <slot></slot>
+            </h3>
+            <button>buttn</button>
+            <p>hello</p>
+        </div>
+    `, 
+    initial: (root) => {
+        console.log(root);
+        root.querySelector('.somethinfg')
+    }, 
+    onMount: (root) => {
+        console.log(root);
+        root.querySelector('.somethinfg')
+        root.querySelector('.somethinfg')
+
+    }, 
+    onDismount: (root) => {
+        console.log(root);
+        root.querySelector('.somethinfg')
+    }, 
+    onChangeAtt: (root) => {
+        console.log(root);
+        root.querySelector('.somethinfg')
+    }, 
+    name: 'todo-list',
+    attributes: [] 
 })
