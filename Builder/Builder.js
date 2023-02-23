6aa5d057bc6076351d41d25a4ed236c88adb66ea
Nathaniel <nathaniel.1stacc@gmail.com>
2023-02-23 19:47:48 +0800

    accept minified

--- Builder/Builder.js
+++ Builder/Builder.js
@@ -5,25 +5,8 @@
  */
  
 
-class Builder {
-    createCustomElement({style, template, script, data}) {
-        const newElement = document.createElement('template')
-        newElement.innerHTML = '<style>'+style+'</style>' + template
-        customElements.define(data.name, class extends HTMLElement {
-            constructor() {
-                super()
-                this.attachShadow({mode: 'open'}) 
-                this.shadowRoot.appendChild(newElement.content.cloneNode(true)) 
-                script.initial(this.shadowRoot, this) 
-            } 
-            connectedCallback() { if(script.onMount) script.onMount(this) }
-            disconnectedCallback() { if(script.onDismount) script.onDismount(this) }
-            adoptedCallback() {if(script.onMove) script.onMove(this) }
-            static get observedAttributes() { return data.attributes } 
-            attributeChangedCallback(name, oldValue, newValue) { if(script.onChangeAtt) script.onChangeAtt(name, oldValue, newValue, this) }
-        })
-    }
-    createCustomElementtest({root, initial, name, obsAtt, onMount, onChangeAtt, onDismount, onMove}) {
+class Builder { 
+    createCustomElement({root, initial, name, obsAtt, onMount, onChangeAtt, onDismount, onMove}) {
         const newElement = document.createElement('template')
         newElement.innerHTML = root
         customElements.define(name, class extends HTMLElement {
