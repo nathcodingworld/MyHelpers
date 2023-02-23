14ce8d458a14de5fec4c7ecf577bc17533bb5977
Nathaniel <nathaniel.1stacc@gmail.com>
2023-02-24 06:06:46 +0800

    add js version

---
+++ Renderer/Rendererraw.js
@@ -0,0 +1,155 @@
+
+class Renderer {
+    media = { xxl: 1400, xl: 1200, lg: 992, md: 768, sm: 576 }
+    mediaCollection= []
+    constructor({ media, targets}) {
+      if(media)this.media = media;
+      if(targets)this.mediaCollection = targets;
+    }
+    mediaQueries(queries) {
+      queries.forEach((query) => this.mediaQuery(query));
+    }
+    mediaQuery({ query, run }) {
+      const [filter, mq] = query.split(":");
+      const windowWidth = window.innerWidth;
+      const [min, max] = mq.split("-");   
+      switch (filter) {
+        case "min":
+          if ( windowWidth >= this.media[mq]) run();
+          break;
+        case "max":
+          if ( windowWidth < this.media[mq]) run();
+          break;
+        case "between":
+          if (windowWidth >= this.media[min] && windowWidth < this.media[max]) run();
+          break;
+        case "around":
+          if ( windowWidth < this.media[min] || windowWidth >= this.media[max]) run();
+          break;
+        default:
+          console.log("not valid query", query);
+          break;
+      }
+      return this;
+    }
+    tableSpliter({ tableid, tdFrom, tdTo }) {
+      const table = document.getElementById(tableid);
+      const targetHeadTr = table.querySelector("thead > tr:not(.splitedtable thead tr)");
+      const targetBodyTr = table.querySelectorAll("tbody > tr:not(.splitedtable tbody tr)");
+      const splitedTh = cutter(targetHeadTr);
+      targetBodyTr?.forEach((tr) => {
+        const splitedTd = cutter(tr);
+        const splitedTr = document.createElement("tr");
+        splitedTr.innerHTML = `
+                  <td colspan=${tdTo - tdFrom + 1}>
+                    <table class="table splitedtable">
+                      <thead>
+                          <tr>
+                          ${splitedTh.innerHTML}
+                          </tr>
+                      </thead>
+                      <tbody>
+                          <tr>
+                          ${splitedTd.innerHTML}
+                          </tr>
+                      </tbody>
+                    </table>
+                  </td>
+               `;
+        tr.insertAdjacentElement("afterend", splitedTr);
+      });
+      function cutter(tr) {
+        const trow = document.createElement("tr");
+        if (tr.childElementCount >= tdTo)
+          for (let i = tdFrom - 1; i < tdTo; i++) {
+            trow.append(tr.children[tdFrom - 1]);
+          }
+        return trow;
+      }
+    }
+    displayToggler(target) {
+      const [selector, display] = target.split(":");
+      const element = document.querySelector(selector);
+      if (element && display == "toggle") {
+        if (element.style.display != "none") element.style.display = "none";
+        else element.style.display = "block";
+      } else if (element) element.style.display = display;
+    }
+    classToggler(target) {
+      const [type, current, change, selector] = target.split(":");
+      const element = document.querySelector(selector);
+      if (element && type == "toggle") {
+        if (element.classList.contains(current)) {
+          element.classList.remove(current);
+          element.classList.add(change);
+        } else {
+          element.classList.remove(change);
+          element.classList.add(current);
+        }
+      } else if (element) {
+        element.classList.remove(current);
+        element.classList.add(change);
+      }
+    }
+    getElement(target, parent = null) {
+      let targetDocument = document;
+      if (parent) targetDocument = parent;
+      const [type, selector] = target.split(":");
+      let element
+      switch (type) {
+        case "ByClass":
+          element = targetDocument.querySelector("." + selector);
+          return element;
+        case "ById":
+          element = targetDocument.getElementById(selector);
+          return element;
+        case "sByClass":
+          element = targetDocument.querySelectorAll("." + selector);
+          return element;
+        case "BySelector":
+          element = targetDocument.querySelector(selector);
+          return element;
+        case "sBySelector":
+          element = targetDocument.querySelectorAll(selector);
+          return element;
+        default:
+          return null;
+      }
+    }
+    setElement({ target, set }) {
+      const [type, selector] = target.split(":");
+      let element
+      switch (type) {
+        case "ByClass":
+          element = document.querySelector("." + selector);
+          if (element) set(element);
+          break;
+        case "ById":
+          element = document.getElementById(selector);
+          if (element) set(element);
+          break;
+        case "sByClass":
+          element = document.querySelectorAll("." + selector);
+          if (element.length > 0)
+            element.forEach((el) => {
+              if (el) set(el);
+            });
+          break;
+        case "BySelector":
+          element = document.querySelector(selector);
+          if (element) set(element);
+          break;
+        case "sBySelector":
+          element = document.querySelectorAll(selector);
+          if (element.length > 0)
+            element.forEach((el) => {
+              if (el) set(el);
+            });
+          break;
+        default:
+          return null;
+      }
+      return this;
+    } 
+  }
+  
