2a329c0f893a2f890a4044d03b0ab2931e50bb87
Nathaniel <nathaniel.1stacc@gmail.com>
2023-02-23 19:24:48 +0800

    add minfied custom element builder

--- Builder/Builder.js
+++ Builder/Builder.js
@@ -1,39 +1,43 @@
-
+/*!
+ * Builder v0.1.0
+ * Helpers Module
+ * by Nathaniel Morales
+ */
  
 
 class Builder {
     createCustomElement({style, template, script, data}) {
         const newElement = document.createElement('template')
         newElement.innerHTML = '<style>'+style+'</style>' + template
-        class customElement extends HTMLElement {
+        customElements.define(data.name, class extends HTMLElement {
             constructor() {
                 super()
-                this.attachShadow({mode: 'open'})
-                this.shadowRoot?.appendChild(newElement.content.cloneNode(true)) 
-                script.initial(this) 
+                this.attachShadow({mode: 'open'}) 
+                this.shadowRoot.appendChild(newElement.content.cloneNode(true)) 
+                script.initial(this.shadowRoot, this) 
             } 
             connectedCallback() { if(script.onMount) script.onMount(this) }
-            disconnectedCallback() { script.onDismount(this) }
+            disconnectedCallback() { if(script.onDismount) script.onDismount(this) }
+            adoptedCallback() {if(script.onMove) script.onMove(this) }
             static get observedAttributes() { return data.attributes } 
-            attributeChangedCallback() { script.onChangeAtt(this) }
-        }
-        window.customElements.define(data.name, customElement)
+            attributeChangedCallback(name, oldValue, newValue) { if(script.onChangeAtt) script.onChangeAtt(name, oldValue, newValue, this) }
+        })
     }
-    createCustomElementtest({root, initial, name, obsAtt, onMount, onChangeAtt, onDismount}) {
+    createCustomElementtest({root, initial, name, obsAtt, onMount, onChangeAtt, onDismount, onMove}) {
         const newElement = document.createElement('template')
-        newElement.innerHTML = '<style>'+style+'</style>' + template
-        class customElement extends HTMLElement {
+        newElement.innerHTML = root
+        customElements.define(name, class extends HTMLElement {
             constructor() {
                 super()
-                this.attachShadow({mode: 'open'})
-                this.shadowRoot?.appendChild(newElement.content.cloneNode(true)) 
-                initial(this) 
+                this.attachShadow({mode: 'open'}) 
+                this.shadowRoot.appendChild(newElement.content.cloneNode(true)) 
+                initial(this.shadowRoot, this)
             } 
             connectedCallback() { if(onMount) onMount(this) }
             disconnectedCallback() { if(onDismount) onDismount(this) }
+            adoptedCallback() {if(onMove) onMove(this) }
             static get observedAttributes() { return obsAtt } 
             attributeChangedCallback() { if(onChangeAtt) onChangeAtt(this) }
-        }
-        window.customElements.define(data.name, customElement)
+        })
     }
 }
