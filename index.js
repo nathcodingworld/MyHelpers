6fbe6d41d5bee3da1ea32097cc14734e2966cc34
Nathaniel <nathaniel.1stacc@gmail.com>
2023-02-26 17:54:30 +0800

    add ignore testing

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const LN = new Listener()
 LN.listens([ 
     {
         element: document.querySelector('.target'),
-        target: 'test:update',
+        instruction: 'test:update',
         callback: (e)=> { 
             e.target.innerText = e.detail.message
             e.target.style.color = 'red'
         }
     }, { 
-        target: '_test:add:all:.target',
+        instruction: 'atest:add:all:.target',
         callback: (e)=> { 
              const div = document.createElement('div')
              div.style.color = 'blue'
@@ -18,7 +18,7 @@ LN.listens([
         }
     }, {
         element: document.getElementById('here'),
-        target: ':click',
+        instruction: ':click',
         callback: (e)=> {  
             LN.trigerEvent('test:update', {
                 message: 'success, print this'
@@ -26,28 +26,28 @@ LN.listens([
         }
     }, {
         element: document.getElementById('add'),
-        target: ':click',
+        instruction: ':click',
         callback: (e)=> { 
-            LN.trigerEvent('test_:add', {
+            LN.trigerEvent('atest:add', {
                 message: 'success, this is added1'
-            }) 
+            },1) 
         } 
     }  , {
         element: document.getElementById('add2'),
-        target: ':click',
+        instruction: ':click',
         callback: (e)=> { 
-            LN.trigerEvent('test_0:add', {
+            LN.trigerEvent('atest:add', {
                 message: 'success, this is added'
-            }) 
+            }, 0) 
         } 
     }  , {
         element: document.getElementById('add3'),
-        target: ':click',
+        instruction: ':click',
         callback: (e)=> { 
             LN.trigerEvents([
-                ['test_0:add', { message: 'success, this is added' }],
-                ['test_1:add', { message: 'success, this is added' }],
-                ['test_2:add', { message: 'success, this is added' }]
+                ['atest:add', { message: 'success, this is added' }, 0],
+                ['atest:add', { message: 'success, this is added' }, 1],
+                ['atest:add', { message: 'success, this is added' }, 2]
             ]) 
         } 
     }  
@@ -91,7 +91,7 @@ LN.listens([
 // }, document.getElementById('add3'))
 
    const test = {
-    asd_: ["01", '02']
+    $asd_: ["01", '02']
    }
 
-   console.log(test['asd_']['1']);
+   
