393d1c1cfdd9064689b4442c9a95b410dd47b028
Nathaniel <nathaniel.1stacc@gmail.com>
2023-03-01 04:26:11 +0800

    test success; all three observers

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
 const OB = new Observer()
 
-
-OB.observeIntersection({
-    instructions: 'all:.card',
-    onIntersect: (entry) => {
-        if(entry.isIntersecting) entry.target.classList.remove('hidden')
-        else entry.target.classList.add('hidden')
-    },
-    options: {
-        rootMargin: '100px',
-    }
+const input = document.querySelectorAll('.card input')  
+const paper = document.querySelectorAll('.text')  
+input.forEach((el, i)=> {
+    el.addEventListener('input', (e)=> { 
+        paper[i].innerText = e.target.value
+    })
 })
 
+function fade (entry) {
+    if(entry.isIntersecting) {
+        entry.target.classList.remove('hidden')
+    } else entry.target.classList.add('hidden') 
+}
+
+console.time('observe')
  
+OB.observeAll([
+    {
+        instructions: ':intersection:all:.card',
+        callback: fade
+    },{
+        instructions: ':intersection:all:.paper',
+        callback: fade
+    },{
+        instructions: ':resize:all:.area',
+        callback: entry => {
+            if(entry.contentRect.width<300) {
+                entry.target.style.background = 'blue'
+            }
+        }
+    },{
+        instructions: ':mutation:all:.paper',
+        callback: entry => {
+            console.log(entry);
+        },
+        options: {
+            subtree: true,
+            childList: true
+        }
+    }
+])
+ 
+
+
+ 
+ console.timeEnd('observe')
