d15c4ec69c7241ba40df19eccafe2eae15f70e13
Nathaniel <nathaniel.1stacc@gmail.com>
2023-04-09 22:49:14 +0800

    additionals

--- Keeper/Keeper.js
+++ Keeper/Keeper.js
@@ -28,25 +28,34 @@ class Keeper {
         this.storage[name] = data
         return this
     }
-    getLocations() {
+    getData() {
         const search = window.location.search
         const keepdata = new URLSearchParams(search).get('keepdata') 
         const data = decodeURIComponent(keepdata)
          console.log(JSON.parse(data));
     }
     setLocations(anchor, locs = undefined) { 
-        const sethref = (anchor) => { 
+        const sethref = (a) => { 
             let datas = {}  
-            if(locs) locs.split(':').forEach(loc=> datas[loc] = this.storage[loc])
-            console.log(datas, datas.length);
+            if(locs) locs.split(':').forEach(loc=> datas[loc] = this.storage[loc]) 
             const data = encodeURIComponent(JSON.stringify(locs ? datas : this.storage))
-            const params = new URLSearchParams(anchor.search); 
+            const params = new URLSearchParams(a.search); 
             if(params.has('keepdata')) params.delete('keepdata')
             params.append("keepdata", data); 
-            anchor.href = anchor.pathname + '?' + params.toString()
+            a.href = a.pathname + '?' + params.toString()
         }
         if(anchor instanceof HTMLAnchorElement) sethref(anchor)
         else if(anchor instanceof String) document.querySelectorAll(anchor).forEach(el=> sethref(el) )
         else window.location.search = 'keepdata='+data 
     } 
+    setPath(ints) {
+        const [key, name] = ints.split(':')
+        const obj = this.storage[key]
+        if(obj) for (const k in obj) {
+            if (Object.hasOwnProperty.call(obj, k)) {
+                const el = obj[k];
+                
+            }
+        }
+    }
 }
