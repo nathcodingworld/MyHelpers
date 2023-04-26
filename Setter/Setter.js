298eca9d2a82f125a90ccfd6493090a5fe12708f
Nathaniel <nathaniel.1stacc@gmail.com>
2023-04-26 11:39:40 +0800

    to merge to main

--- Setter/Setter.js
+++ Setter/Setter.js
@@ -1,7 +1,7 @@
 /**
  * Setter v0.1.0
- * Helpers Module
- * by Nathaniel Morales
+ * My Helpers
+ * Nathaniel Morales
  */
 class Setter {
     media = { xxl: 1400, xl: 1200, lg: 992, md: 768, sm: 576 } 
